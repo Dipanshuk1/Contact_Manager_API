c5d8109c4569c473131631be8236201143fc2931
Dipanshuk1 <dipanshukumar1936@gmail.com>
2025-09-24 23:23:38 +0530

    Updated server.js

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,15 @@ import cors from "cors";
 import dotenv from "dotenv";
 
 import connectDB from "./config/db.js";
+import contactRoutes from "./routes/contactRoutes.js";
 
 const app=express();
 dotenv.config();
 
 app.use(express.json());
 app.use(cors());
+
+app.use("/api/contacts", contactRoutes);
 
 // mongoose.connect(process.env.MONGO_URI)
 // .then(()=>{
